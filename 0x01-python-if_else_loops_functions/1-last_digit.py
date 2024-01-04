b4dd0c498800f401a991dca7104505da7b6a6880
Matt Nyaseda <mattkeneth@gmail.com>
2024-01-04 18:27:54 +0000

    fix task 1 - negation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,21 +2,21 @@
 import random
 number = random.randint(-10000, 10000)
 if number > 0:
-    last_digit = number % 10
+    l_d = number % 10
 
-    if last_digit > 5:
-        print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
-    elif last_digit == 0:
-        print("Last digit of {} is {} and is 0".format(number, last_digit))
-    elif last_digit < 6 and last_digit != 0:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
+    if l_d > 5:
+        print("Last digit of {} is {} and is greater than 5".format(number, l_d))
+    elif l_d == 0:
+        print("Last digit of {} is {} and is 0".format(number, l_d))
+    elif l_d < 6 and l_d != 0:
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, l_d))
 
 
 if number < 0:
-    last_digit = (number % 10) * -1
-    if last_digit > 5:
-        print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
-    elif last_digit == 0:
-        print("Last digit of {} is {} and is 0".format(number, last_digit))
-    elif last_digit < 6 and last_digit != 0:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
+    l_d = (number * -1) % 10
+    if l_d > 5:
+        print("Last digit of {} is {} and is greater than 5".format(number, l_d))
+    elif l_d == 0:
+        print("Last digit of {} is {} and is 0".format(number, l_d))
+    elif l_d < 6 and l_d != 0:
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, l_d))
---
+++ 0x01-python-if_else_loops_functions/pr.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+num = -7552
+l = (num * -1) % 10
+print(l)
