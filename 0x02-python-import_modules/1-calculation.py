371244fa67065f1d17039117c40af2be63538c4a
Matt Nyaseda <mattkeneth@gmail.com>
2024-01-09 18:05:47 +0000

    task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+res0 = add(a, b)
+res1 = sub(a, b)
+res2 = mul(a, b)
+res3 = div(a, b)
+
+if __name__ = "__main__":
+    print("{0} + {1} = {2:d}".format(a, b, res0))
+    print("{0} + {1} = {2:d}".format(a, b, res1))
+    print("{0} + {1} = {2:d}".format(a, b, res2))
+    print("{0} + {1} = {2:d}".format(a, b, res3))
