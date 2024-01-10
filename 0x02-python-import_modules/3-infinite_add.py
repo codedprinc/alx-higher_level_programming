47e8d3149e2d54387ec9d6d7d009092950ae160b
Matt Nyaseda <mattkeneth@gmail.com>
2024-01-10 16:10:19 +0000

    fix task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,5 @@ if __name__ == "__main__":
     n = len(sys.argv)
     res = 0
     for y in range(1, n):
-        #print("{:d}".format(int(sys.argv[y])))
         res += (int(sys.argv[y]))
     print("{:d}".format(int(res)))
