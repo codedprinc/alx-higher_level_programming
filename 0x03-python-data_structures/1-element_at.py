c7effd7f6eac3ae8ad50fbfc44daf0caa305c7b7
Matt Nyaseda <mattkeneth@gmail.com>
2024-01-11 19:06:09 +0000

    fix task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    l = len(my_list)
+    length = len(my_list)
     if idx < 0:
         return None
-    elif idx > l:
+    elif idx > length:
         return None
     elif idx >= 0:
-        ans = "{}".format(my_list[idx])
-        return ans
+        return my_list[idx]
