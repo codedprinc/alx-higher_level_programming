e1aa5ef4a37f41c650b246282020359dca3f3a91
Matt Nyaseda <mattkeneth@gmail.com>
2024-01-27 13:33:52 +0000

    fix task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     length = len(my_list)
-    n_list = my_list.copy()
-    l_list = []
 
     if idx < 0 or idx >= length:
-        return n_list
+        return my_list
     else:
-        temp = n_list[idx]
-        for i in range(length):
-            if n_list[i] != temp:
-                l_list.append(n_list[i])
-
-        return l_list
+        del my_list[idx]
+        return my_list
