76aa4cf2bb8aa299fae7a9dad5d8511823862fc7
Matt Nyaseda <mattkeneth@gmail.com>
2024-03-01 09:40:23 +0000

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Has a class rectangle"""
+
+
+class Rectangle:
+    """Defines a rectangle based on 0-rectangle.py"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes the class with two arguments
+
+        Args:
+          width (:obj:`int`, optional): Rectangle' s width.
+          height (:obj:`int`, optional): Rectangle ' s height.
+
+        Raises:
+           TypeError: If `width` or `height` type is not `int`
+
+           ValueError: If `width` or `height` type is less than `0`
+
+        """
+
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+
+        if width < 0:
+            raise ValueError('width ust be >= 0')
+
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+
+        if height < 0:
+            raise ValueError('height ust be >= 0')
+
+        self.__width = width
+        self.__height = height
+
+
+    @property
+    def width(self):
+        """int: retireves the value of width"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+
+        if value < 0:
+            raise ValueError('width ust be >= 0')
+
+        self.__width = value
+
+
+    @property
+    def height(self):
+        """int: retireves the value of height"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+
+        if value < 0:
+            raise ValueError('width ust be >= 0')
+
+        self.__height = value
