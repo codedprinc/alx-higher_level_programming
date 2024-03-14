89aa79f7ccad5b34b06a1f7215a60dd7bd1786d4
Matt Nyaseda <mattkeneth@gmail.com>
2024-03-14 12:19:55 +0000

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pytyhon3
+"""
+A module to print a list in ascending order
+"""
+
+
+class MyList(list):
+    """
+    A class to customize the list class
+
+    """
+
+    def print_sorted(self):
+        """ First sorts the list then prints it"""
+
+        if issubclass(MyList, list):
+            print(sorted(self))
