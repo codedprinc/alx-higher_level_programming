8667adae0aaefbd200399ddfbdeaf0ade0285996
Matt Nyaseda <mattkeneth@gmail.com>
2024-03-14 15:53:30 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class `BaseGeometry`"""
+
+
+class BaseGeometry:
+    """has a function area"""
+
+    def area(self):
+        """
+        Raises:
+            Exception: area() is not implemented.
+        """
+
+        raise Exception('area() is not implemented')
