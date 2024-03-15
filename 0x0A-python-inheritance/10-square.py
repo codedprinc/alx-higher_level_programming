411718c1cd1830b9b1276f1f303dce8ddb4f4771
Matt Nyaseda <mattkeneth@gmail.com>
2024-03-15 11:03:22 +0000

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-""" Contains a class `square`"""
+""" Contains a class `square`
+
+"""
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle.py').Rectangle
+
 
 class Square(Rectangle):
     """ Sqaure that inherits properties from class `Rectangle`"""
 
     def __init__(self, size):
         """
+        Args:
+            size (int): the size of the square
 
         """
 
