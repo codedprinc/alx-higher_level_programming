7db2a0dc0ceb0a8d94691234e8bd0cd4fe42876c
Matt Nyaseda <131536270+codedprinc@users.noreply.github.com>
2024-03-18 10:19:33 +0300

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,26 +1,78 @@
 #!/usr/bin/python3
-""" Contains a class `square`
-
+"""
+This module contains class definition for:
+    - BaseGeometry
+    - Rectangle - Inherits from BaseGeometry
+    - Square - Inherits from Rectangle
 """
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-Rectangle = __import__('9-rectangle.py').Rectangle
+
+class BaseGeometry:
+    """
+    Defines BaseGeometry
+    """
+
+    def area(self):
+        """
+        Raises an exception
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates value
+        """
+
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+    Defines Rectangle
+    """
+
+    def __init__(self, width, height):
+        """Initializes instance attributes"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """Returns unofficial string representaion of Rectangle inst."""
+
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def __repr__(self):
+        """Returns the official string representation"""
+
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def area(self):
+        """Returns the area of the Rectangle instance"""
+
+        return self.__width * self.__height
 
 
 class Square(Rectangle):
-    """ Sqaure that inherits properties from class `Rectangle`"""
+    """
+    Defines a Square
+    """
 
     def __init__(self, size):
-        """
-        Args:
-            size (int): the size of the square
+        """Initializes instance attributes"""
 
-        """
-
-        self.integer_validator("size", size)
-
+        super().__init__(width=size, height=size)
+        self.integer_validator("length", size)
         self.__size = size
 
     def area(self):
-        """ """
-        return (self.__size ** self.__size)
+        """Returns area of the Square instance"""
+
+        return self.__size * self.__size
