e763d7076ba30a73d0fce5ce16b6ba400ca4dff0
Matt Nyaseda <mattkeneth@gmail.com>
2024-03-22 11:39:30 +0000

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 0. read file"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (`UTF8`) and prints to stdout
+
+    """
+
+    with open(filename, encoding="utf-8") as f:
+        data = f.read()
+
+        print(data)
