bc76ebaf9cfc1b166ca2e3d393d57c9c61fabf09
Matt Nyaseda <mattkeneth@gmail.com>
2024-03-22 13:00:36 +0000

    fix task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -10,5 +10,8 @@ def class_to_json(obj):
     returns the dictionary description with simple data structure
     (list, dictionary, string, integer and boolean)
     for JSON serialization of an object
+
+    Returns:
+       `dict` : description of the data structures in the class
     """
-    return json.dumps(obj)
+    return (vars(obj))
