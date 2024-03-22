187aa12085f1a6e43d96741f80788e7326c7709c
Matt Nyaseda <mattkeneth@gmail.com>
2024-03-22 11:42:49 +0000

    fix task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,4 +11,5 @@ def read_file(filename=""):
     with open(filename, encoding="utf-8") as f:
         data = f.read()
 
-        print(data)
+        for line in data:
+            print(line, end='')
