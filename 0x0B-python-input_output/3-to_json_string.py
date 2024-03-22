e658de0d35da498af5d11b7080eb2403453d9c4f
Matt Nyaseda <mattkeneth@gmail.com>
2024-03-22 12:12:02 +0000

    task 4

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -6,6 +6,10 @@ import json
 
 def to_json_string(my_obj):
     """ returns the JSON representation of an object(string):
+
+    Returns:
+        json version of the `my_obj`.
+
     """
 
     return json.dumps(my_obj)
