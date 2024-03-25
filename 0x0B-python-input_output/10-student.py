618abcc25b19cdb9149d14e703e3de465fa71463
Matt Nyaseda <mattkeneth@gmail.com>
2024-03-25 10:17:43 +0000

    too many blank lines

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""10. Student to JSON with filter """
+
+
+class Student:
+    """
+    my class student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantates the class `Student`
+
+        Args:
+           first_name (str) : Student' s first name
+           last_name (str) : Student' s last name.
+           age (int) : Student' s age
+
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student instance
+
+        """
+
+        return self.__dict__
