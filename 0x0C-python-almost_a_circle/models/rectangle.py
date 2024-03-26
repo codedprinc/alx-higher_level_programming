2599ce11bf49d6d4966a7ce9f89ccbd27205a4dc
Matt Nyaseda <mattkeneth@gmail.com>
2024-03-26 08:05:49 +0000

    fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,10 +86,10 @@ class Rectangle(Base):
           TypeError: If `attr_i` is not `int`.
         """
         if type(value) is not int:
-            raise TypeError(param  + 'must be an integer')
+            raise TypeError(param + ' must be an integer')
 
         if value <= 0 and param in ('width', 'height'):
-            raise ValueError(param + 'must be > 0')
+            raise ValueError(param + ' must be > 0')
 
         if value < 0 and param in ('x', 'y'):
-            raise ValueError(param + 'must be >= 0')
+            raise ValueError(param + ' must be >= 0')
