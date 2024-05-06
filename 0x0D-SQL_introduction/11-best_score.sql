b7531dbba74a8e9e4e983a30992067d5e3f1039c
Matt Nyaseda <mattkeneth@gmail.com>
2024-05-06 09:30:05 +0000

    remove '' near count and backticks

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -4,5 +4,5 @@
 -- The database name will be passed as an argument of the mysql command.
 SELECT score, name
 FROM second_table
-     WHERE `score` >= 10
+     WHERE score >= 10
      ORDER BY score DESC;
